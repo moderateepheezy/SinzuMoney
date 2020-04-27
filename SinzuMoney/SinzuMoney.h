fd9942c963eec723573afd46264bc4ae9ed5ac04
Afees Lawal <moderateepheezy@gmail.com>
2020-04-27 21:20:24 +0100

    Initial Commit

---
+++ SinzuMoney/SinzuMoney.h
@@ -0,0 +1,19 @@
+//
+//  SinzuMoney.h
+//  SinzuMoney
+//
+//  Created by Afees Lawal on 27/04/2020.
+//  Copyright © 2020 Afees Lawal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SinzuMoney.
+FOUNDATION_EXPORT double SinzuMoneyVersionNumber;
+
+//! Project version string for SinzuMoney.
+FOUNDATION_EXPORT const unsigned char SinzuMoneyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SinzuMoney/PublicHeader.h>
+
+
