dc52637d33f4d2c17f1164196ec100a446098e88
Afees Lawal <moderateepheezy@gmail.com>
2020-05-02 04:03:31 +0100

    Intitial setup 🔥

---
+++ SinzuMoneyExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SinzuMoneyExamples
+//
+//  Created by Afees Lawal on 02/05/2020.
+//  Copyright © 2020 Afees Lawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
