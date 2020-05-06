ac543f859e2b3789809b7c6ff6a3016969f7e17c
Afees Lawal <moderateepheezy@gmail.com>
2020-05-06 06:08:41 +0100

    add simple test cases

--- SinzuMoneyTests/SinzuMoneyTests.swift
+++ SinzuMoneyTests/SinzuMoneyTests.swift
@@ -11,24 +11,14 @@ import XCTest
 
 class SinzuMoneyTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func testPositiveMoney() {
+        let money = Money.from(value: -200.34).positive
+        XCTAssertEqual(money.doubleValue, 200.34)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testZeroMoney() {
+        let money = Money.zero
+        XCTAssertEqual(money.doubleValue, 0)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
